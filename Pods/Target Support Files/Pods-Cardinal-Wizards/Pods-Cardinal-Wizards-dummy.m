d73f60c01e63b16099a14b6c913f77a6485ff1f4
juliascript <juliageist23@gmail.com>
2017-10-15 20:56:17 -0700

    jk

--- Pods/Target Support Files/Pods-Cardinal-Wizards/Pods-Cardinal-Wizards-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cardinal_Wizards : NSObject
-@end
-@implementation PodsDummy_Pods_Cardinal_Wizards
-@end
--- Testing Features/Cardinal Wizards/Pods/Target Support Files/Pods-Cardinal WizardsTests/Pods-Cardinal WizardsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cardinal_WizardsTests : NSObject
-@end
-@implementation PodsDummy_Pods_Cardinal_WizardsTests
-@end
